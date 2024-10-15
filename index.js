29e3aaacbebe2067c8e2b2390314c5058882121d
Maxence Choiselle <mchoiselle@gmail.com>
2024-10-15 11:22:30 +0200

    [Feat] : Résolution workshop chalk

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const wilder1 = "Jeremy Perez"
+const wilder2 = "Flavien Rousseau"
+const wilder3 = "Vincent Ching"
+const wilder4 = "Jeremy Tichané"
+
+console.log("Mes compagnons d'armes sont" + " "+ chalk.blue(wilder1) + " "+ "ainsi que" + " "+ chalk.red(wilder2)+ " "+ "mais également "+ " "+ chalk.yellow(wilder3) + " "+ " et pour finir "+ " "+ chalk.green(wilder4)+" "+ "!!!")
